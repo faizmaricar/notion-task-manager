6d1fd0b6fdb7688444e2b1e71927cc31615c2744
Faiz Maricar <maricar.faiz@gmail.com>
2023-02-13 17:37:54 +0800

    set time to normal

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ switch (command) {
 
     for (let day = 0; day < 5; day++) {
       const date = addDays(monday, day);
-      date.setHours(14, 0);
+      date.setHours(6, 0);
       addEvent("Running", date, addHours(date, 1));
 
-      date.setHours(16, 30);
+      date.setHours(8, 30);
       addEvent("Work", date, addHours(date, 9));
     }
     break;
