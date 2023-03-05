211a5ca49d95bae5edc09f3619d4a9d491682886
Faiz Maricar <maricar.faiz@gmail.com>
2023-03-05 11:36:36 +0800

    filter weekends

--- src/schedule/updateWeekdaySchedule.js
+++ src/schedule/updateWeekdaySchedule.js
@@ -1,6 +1,8 @@
 import { dayNumbers } from "../utils/index.js";
 import updateSchedule from "./updateSchedule.js";
 
+const weekDayNumbers = dayNumbers.weekdays();
+
 export default function updateWeekdaySchedule(weekdays) {
-  for (let day in dayNumbers) updateSchedule(weekdays, day);
+  for (let day in weekDayNumbers) updateSchedule(weekdays, day);
 }
--- src/utils/getDayNumber.js
+++ src/utils/getDayNumber.js
@@ -6,6 +6,11 @@ export const dayNumbers = {
   thursday: 4,
   friday: 5,
   saturday: 6,
+  weekdays: function () {
+    return Object.keys(this)
+      .filter((key) => !["saturday", "sunday", "weekdays"].includes(key))
+      .reduce((a, b) => ({ ...a, [b]: this[b] }), {});
+  },
 };
 
 export default function getDayNumber(day) {
