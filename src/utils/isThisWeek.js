59add6ce676606cec87cd1173a9d8cc6ba3a9e0a
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-21 19:03:39 +0800

    update to every

--- src/utils/isThisWeek.js
+++ src/utils/isThisWeek.js
@@ -1,8 +1,9 @@
 import { differenceInWeeks, parse, getDay } from "date-fns";
 
 export default function isThisWeek(startTime, from, every, day) {
-  return (
-    differenceInWeeks(startTime, parse(from, "dd/MM/yyyy", new Date())) ===
-      every && getDay(startTime) === day
+  const weeks = differenceInWeeks(
+    startTime,
+    parse(from, "dd/MM/yyyy", new Date())
   );
+  return weeks % every === 0 && getDay(startTime) === day;
 }
