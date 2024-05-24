5476a4d8a6bec3c3b61f5db287e77383ff16bf57
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-24 17:43:21 +0800

    add days no start

--- src/utils/getStartAndEndTime.js
+++ src/utils/getStartAndEndTime.js
@@ -2,7 +2,7 @@ import { addDays, addMinutes, format, parse } from "date-fns";
 
 export default function getStartAndEndTime(start, duration) {
   const today = new Date();
-  if (!start && !duration) return [today, null];
+  if (!start && !duration) return [addDays(today, 1), null];
 
   let todayString = format(today, "dd/MM/yyyy");
   todayString = `${todayString} ${start}`;
