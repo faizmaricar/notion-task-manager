358e778d8f08497402b3e8ad15adc43a067a9901
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-25 21:52:40 +0800

    check for syuruk

--- src/schedule/updatePrayerSchedule.js
+++ src/schedule/updatePrayerSchedule.js
@@ -11,7 +11,7 @@ export default async function updatePrayerschedule() {
     const [startTime] = createStartAndEndTime(
       PrayerDate,
       time,
-      timing === "Subuh"
+      timing === "Subuh" || timing === "Syuruk"
     );
     addPage(timing, "Event", startTime);
   }
