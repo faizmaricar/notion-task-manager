0c3127a016b808ea3f24f185178c26cbc03c0c0a
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-25 19:59:09 +0800

    add break

--- src/schedule/updateSchedule.js
+++ src/schedule/updateSchedule.js
@@ -2,11 +2,10 @@ import {
   getPath,
   readYaml,
   getStartAndEndTime,
-  isWeekday,
   isThisWeek,
 } from "../utils/index.js";
 import { addPage } from "../notion/index.js";
-import { addMilliseconds, getDay, isWeekend } from "date-fns";
+import { getDay, isWeekend } from "date-fns";
 
 export default function updateSchedule() {
   const schedulePath = getPath("schedule.yml");
@@ -25,12 +24,9 @@ export default function updateSchedule() {
     } = item;
     const [startTime, endTime] = getStartAndEndTime(start, duration);
 
-    const now = new Date();
-    const sgDate = addMilliseconds(now, 8 * 60 * 60 * 1000);
-
     switch (frequency) {
       case "monthly":
-        if (getDay(sgDate) === day) addPage(title, type, startTime, endTime);
+        if (getDay(startTime) === day) addPage(title, type, startTime, endTime);
       case "weekly":
         if (isThisWeek(startTime, from, every, day))
           addPage(title, type, startTime, endTime);
@@ -39,9 +35,10 @@ export default function updateSchedule() {
         addPage(title, type, startTime, endTime);
         break;
       case "weekdays":
-        if (!isWeekend(sgDate)) addPage(title, type, startTime, endTime);
+        if (!isWeekend(startTime)) addPage(title, type, startTime, endTime);
+        break;
       case "weekends":
-        if (isWeekend(sgDate)) addPage(title, type, startTime, endTime);
+        if (isWeekend(startTime)) addPage(title, type, startTime, endTime);
         break;
       default:
         break;
