3ef5d5afc94726daef34e3e877865516c7d4e1fc
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-25 18:07:15 +0800

    debug date

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import updatePrayerschedule from "./src/schedule/updatePrayerSchedule.js";
 import updateSchedule from "./src/schedule/updateSchedule.js";
 
-updatePrayerschedule();
-updateSchedule();
+// updatePrayerschedule();
+//updateSchedule();
+
+console.log("[DEBUG]:", new Date());
