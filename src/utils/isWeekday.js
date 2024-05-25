cf31ddb6832b1b00be3ec96ad6ae38fb77c8bc18
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-25 18:44:41 +0800

    less than 5

--- src/utils/isWeekday.js
+++ src/utils/isWeekday.js
@@ -1,6 +1,5 @@
-import { addMilliseconds, getDay } from "date-fns";
+import { getDay } from "date-fns";
 
 export default function isWeekday(date) {
-  const singaporeDate = addMilliseconds(date, 8 * 60 * 60 * 1000);
-  return getDay(singaporeDate) < 6;
+  return getDay(date) < 5;
 }
