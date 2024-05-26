2599a93aedc6ac8885afda4207d48a33f1dfb13d
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-26 12:23:09 +0800

    update to sg time

--- src/utils/getStartAndEndTime.js
+++ src/utils/getStartAndEndTime.js
@@ -2,7 +2,13 @@ import { addMinutes, parse } from "date-fns";
 
 export default function getStartAndEndTime(start, duration) {
   const today = new Date();
-  const startTime = parse(start, "HHmm", new Date());
+
+  const startTime = parse(
+    start,
+    "HHmm",
+    new Date(today.getTime() + 8 * 60 * 60 * 1000)
+  );
+
   const endTime = addMinutes(startTime, duration);
 
   if (!start && !duration) return [today, null];
