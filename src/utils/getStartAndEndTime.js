adc4ab1faca521d8336286d48c06b809cfb13c25
Faiz Maricar <maricar.faiz@gmail.com>
2024-05-26 13:40:24 +0800

    remove colon

--- src/utils/getStartAndEndTime.js
+++ src/utils/getStartAndEndTime.js
@@ -2,7 +2,7 @@ import { addMinutes, parse } from "date-fns";
 
 export default function getStartAndEndTime(date, start, duration) {
   const today = new Date();
-  const startTime = parse(`${date} ${start}`, "dd MMMM yyyy HH:mm", today);
+  const startTime = parse(`${date} ${start}`, "dd MMMM yyyy HHmm", today);
   const endTime = addMinutes(startTime, duration);
 
   if (!start && !duration) return [today, null];
